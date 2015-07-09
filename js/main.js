32187bdf720876085c69582e8bcad5ee2a287e58
zhangping <ping.zhang@bowenpay.com>
2015-07-09 10:54:05 +0800

    '类似格林深瞳，基本框架完成，但不兼容360，需改动'

---
+++ js/main.js
@@ -0,0 +1,47 @@
+/**
+ * Created by yijingping on 2015/6/3.
+ */
+
+var demo=new Vue({
+    el: '#show',
+    data:{
+        title:"波纹积分",
+
+        page1_1:"折扣、换钱、消费，就用波纹积分",
+
+        page2_1_title:"波纹积分",
+        page2_1_1:"折扣、换钱、消费，就用波纹积分",
+        page2_2_title:"波纹，引领建立新型积分体系",
+        page2_2_1:"波纹帮每个有信用的个体建立一套积分，可支付可流通",
+
+        page3_1_title:"加入波纹的商户越来越多",
+        page3_1_1:"您能享受到的服务也越来越广,越来越多的商户在波纹发放积分",
+
+        page3_2_title:"在波纹，您可以享受优质服务",
+        page3_2_1:"积分可以相互兑换，无用积分兑换成您需要的积分",
+
+        page3_3_title:"波纹积分，是一个积分消费平台",
+        page3_3_1:"波纹积分，是一个积分消费平台",
+        page3_3_2:"波纹积分，是一个积分消费平台",
+
+        page5_1_title:"加入波纹",
+        page5_1_content:"北京波纹积分有限公司，是一个互联网金融公司。在这里，你定能找到价值所在。",
+        page5_1_mailbox:"简历请投递到:hr@bowenpay.com",
+        page5_1_image:"./img/5_fn1.1.png",
+        //page5_1_color:"",
+
+        //page5_2_color:"",
+        page5_3_title:"安卓工程师",
+        page5_3_content:"独立 负责安卓 APP 开发工作；代码实现UI/UX设计师及产品经理的效果图及交互效果；优化代码，提升用户体验。",
+        page5_3_image:"./img/5_fn3.1.png",
+        //page5_3_color:"",
+        page5_4_title:"IOS工程师",
+        page5_4_content:"负责移动端相关产品架构设计和研发；测试和修正开发中发现的问题；学习和研究新技术以满足产品的需求,确保iOS项目开发进度与上线。",
+        page5_4_image:"./img/compute.png",
+        //page5_4_color:"",
+        page5_5_title:"BD工程师",
+        page5_5_content:"负责波纹在线下餐饮，服务等行业的推广；负责与商户合作中的线下运营问题。",
+        page5_5_image:"./img/5_fn6.1.png",
+
+    }
+});
